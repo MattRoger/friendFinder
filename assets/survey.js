92db577db13450e3c81551993bc9ab0a5debb1f7
MattRoger <mrogerphotography@gmail.com>
2019-10-13 10:12:29 -0600

    now printing user answers and questions

--- assets/survey.js
+++ assets/survey.js
@@ -1,5 +1,4 @@
 console.log("working")
-var user;
 let x=0;
 const questions=[
     "1",
@@ -13,27 +12,36 @@ const questions=[
     "9",
     "10",
 ];
-const userRespsonses=[]
+const userResponses=[]
+var user;
+const name=[]
+ 
 $("#submitName").on("click", function(){
     event.preventDefault()
     console.log("clicked");
-    user=$(userName).val().trim()
-    console.log(user)
+    user=$("#userName").val().trim()
+    name.push(user)
     askQuestion()
     $("#enterName").empty()
 })
+const UserObject={
+    name:name,
+    answers:userResponses
+}
+
 
 function askQuestion(){
     $("#question").empty()
     var q =questions[x];
     $("#question").append(q)
-   
+    
 }
 answered()
 function answered(){
     $("#answer_div").on("click","button",function(){
         const userResponse=$(this).val()
         console.log(userResponse)
+        userResponses.push(userResponse)
         x+=1;
         if(x===10){
             end()
@@ -45,5 +53,25 @@ function answered(){
 
 
 function end(){
+    console.log(user)
     console.log("the end")
+    console.log(UserObject)
+    var userInfo=$("<div>")
+    const yourName=$("<h1>")
+    yourName.text(UserObject.name)
+    const yourAnswers=$("<ol>")
+    for(i=0;i<questions.length;i++){
+        const q=questions[i]
+       const a=UserObject.answers[i];
+        const qa=$("<li>");
+        qa.text(`${q}
+        You Said ${a}`)
+        yourAnswers.append(qa)
+    }
+    
+    userInfo.append(yourName)
+    userInfo.append(yourAnswers)
+
+    $("#match").append(userInfo)
+    
 }
