a6e6efd72e7e352990fe74425ff88fbb63655f86
MattRoger <mrogerphotography@gmail.com>
2019-11-07 09:46:05 -0700

    try again

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -6,5 +6,8 @@ module.exports = function(app) {
         app.get("*", function(req, res) {
         res.sendFile(path.join(__dirname, "/../public/home.html"));
       });
-    
+      app.get("/", function(req, res) {
+        res.json(path.join(__dirname, "public/home.html"));
+      });
+      
     };
